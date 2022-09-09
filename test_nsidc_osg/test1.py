0e691f80860831e1bcd74e31267babe7c8493546
Philipp S. Arndt <parndt@ucsd.edu>
2022-09-09 01:19:43 -0700

    move input files in python for test

--- test_nsidc_osg/test1.py
+++ test_nsidc_osg/test1.py
@@ -5,17 +5,17 @@ import os
 import rsa
 import argparse
 
+# move input files
+os.system("mkdir misc && mkdir geojsons")
+os.system("mv test1 misc/ && mv test2 misc/ && mv *.geojson geojsons/")
+
+# parse command line arguments
 parser = argparse.ArgumentParser(description='Test script to print some stats for a given ICESat-2 ATL03 granule.')
 parser.add_argument('--granule', type=str, default='ATL03_20210715182907_03381203_005_01.h5',
                     help='the producer_id of the input ATL03 granule')
 parser.add_argument('--polygon', type=str, default='/geojsons/jakobshavn_small.geojson',
                     help='the file path of a geojson file for spatial subsetting')
-
 args = parser.parse_args()
-
-print(args.granule)
-print(args.polygon)
-
 
 gtxs = 'all'
 datadir = '/IS2data'
